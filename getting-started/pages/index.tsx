ddda48d500eb1b6f47bfba5f70a7064130cacdbe
MarcusMystrader - pessoalComputer <mystrader@gmail.com>
2021-06-12 20:50:54 -0300

    versao de exemplo do site oficial, mostrando o darkmode

--- getting-started/pages/index.tsx
+++ getting-started/pages/index.tsx
@@ -1,5 +1,34 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
-const IndexPage = () => <Flex>sdf</Flex>;
+const IndexPage = () => {
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background={formBackground} p={12} rounded={6}>
+        <Heading mb={6}>Log in</Heading>
+        <Input
+          placeholder="mystrader@gmail.com"
+          variant="filled"
+          mb={3}
+          type="email"
+        />
+        <Input placeholder="*******" variant="filled" mb={6} type="password" />
+        <Button mb={6} colorScheme="blue">
+          Log in
+        </Button>
+        <Button onClick={toggleColorMode}>Toggle Color Mode </Button>
+      </Flex>
+    </Flex>
+  );
+};
 
 export default IndexPage;
